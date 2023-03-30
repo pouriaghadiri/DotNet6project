0131663b03f2a9c337ab796656b7d16788103b98
pOri_A <pouria7679@gmail.com>
2023-03-30 18:02:03 +0430

    Create new Controller with View for Leave Types

---
+++ LeaveManagement.Web/Models/LeaveTypeVM.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.Web.Models
+{
+    public class LeaveTypeVM
+    {
+        public int Id{ get; set; }
+        public string Name { get; set; }
+        [Display(Name = "Defaul Number Of Days")]
+        public int DefaultDays { get; set; }
+    }
+}
